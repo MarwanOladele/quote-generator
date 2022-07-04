d317e2c7b2a4d454b41a649490d51d6da0b0f4b0
Oladele Marwan <oladelemarwan@gmail.com>
2022-07-04 19:03:46 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
   const [quote, setQuote] = useState("");
 
-  return <div>App</div>;
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        setQuote(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return <div>{quote}</div>;
 };
 
 export default App;
